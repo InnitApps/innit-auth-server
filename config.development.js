74cedf0cc77a5318bad44042a2c6b477bafe9b23
Rob Wormald <robwormald@gmail.com>
2014-02-07 00:04:44 -0600

    innit ui

---
+++ config.development.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "local-ui": "ui/app",
+  "redis": {
+    "url": "redis://127.0.0.1:6379"
+  }
+}
--- config.production.js
+++ config.production.js
@@ -2,7 +2,7 @@ var url = require('url');
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
 
 module.exports = {
-  "local-ui": "node_modules/innit-auth-server-ui/app",
+  "local-ui": "ui/app",
   "redis": {
     "url": process.env.REDISCLOUD_URL,
     "password" : redisURL.auth.split(":")[1]
