140ae7e16bf96a9ee6dceb20ff7eec379fe33399
Rob Wormald <robwormald@gmail.com>
2014-02-07 22:12:08 -0600

    updating ui

--- config.development.js
+++ config.development.js
@@ -1,5 +1,6 @@
+
 module.exports = {
-  "local-ui": "ui/app",
+  "local-ui": "ui/app", 
   "redis": {
     "url": "redis://pub-redis-11315.us-east-1-4.3.ec2.garantiadata.com:11315/redis-app21986323",
     "auth" : "2oKII7vzYPbvUugg"
