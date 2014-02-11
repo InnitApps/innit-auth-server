c1ec1fbaf2087ac7b145cd63a7cdc8b6b5cd5f0b
Rob Wormald <robwormald@gmail.com>
2014-02-10 23:15:47 -0600

    win

--- config.development.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  "local-ui": "./ui/app", 
-  "redis": {
-    "url": "redis://pub-redis-11315.us-east-1-4.3.ec2.garantiadata.com:11315/redis-app21986323",
-    "auth" : "2oKII7vzYPbvUugg"
-  }
-}
