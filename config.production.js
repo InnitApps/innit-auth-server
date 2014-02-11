5f4c1ffc2025419d73af007887eb4ef51cbbb73a
Rob Wormald <robwormald@gmail.com>
2014-02-10 18:17:14 -0600

    cofig

--- config.production.js
+++ config.production.js
@@ -1,8 +1,9 @@
 var url = require('url');
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var path = require('path')
 
 module.exports = {
-  "local-ui" : "./ui/app", 
+  "local-ui" : path.resolve('ui/dist'), 
   "redis": {
     "url": process.env.REDISCLOUD_URL,
     "password" : redisURL.auth.split(":")[1]
